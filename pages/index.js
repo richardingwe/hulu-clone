1d73a882059e900aa44edbfca4f022551468b889
richardingwe <richardingwe5@gmail.com>
2021-05-04 03:25:10 +0100

    changed the pages title

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,10 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className>
+    <div>
+      <head>
+        <title>Hulu 3.0</title>
+      </head>
       <h1>Hulu</h1>
     </div>
   );
