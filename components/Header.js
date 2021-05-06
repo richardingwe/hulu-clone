d6f89a73f9497ed904be93870f47cbe4a832e161
richardingwe <richardingwe5@gmail.com>
2021-05-06 23:57:06 +0100

    completed and made the header fully responsive.

--- components/Header.js
+++ components/Header.js
@@ -4,14 +4,16 @@ import { BadgeCheckIcon, CollectionIcon, HomeIcon, LightningBoltIcon, SearchIcon
 
 const Header = () => {
     return (
-        <header className=''>
-            <HeaderItem title='HOME' Icon={HomeIcon} />
-            <HeaderItem title='TRENDING' Icon={LightningBoltIcon} />
-            <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} />
-            <HeaderItem title='COLLECTIONS' Icon={CollectionIcon} />
-            <HeaderItem title='SEARCH' Icon={SearchIcon} />
-            <HeaderItem title='ACCOUNT' Icon={UserIcon} />
-            <Image className='object-contain' src='https://links.papareact.com/ua6' width={200} height={200} />
+        <header className='flex flex-col sm:flex-row m-5 justify-between items-center h-auto'>
+            <div className="flex flex-grow justify-evenly max-w-2xl">
+                <HeaderItem title='HOME' Icon={HomeIcon} />
+                <HeaderItem title='TRENDING' Icon={LightningBoltIcon} />
+                <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} />
+                <HeaderItem title='COLLECTIONS' Icon={CollectionIcon} />
+                <HeaderItem title='SEARCH' Icon={SearchIcon} />
+                <HeaderItem title='ACCOUNT' Icon={UserIcon} />
+            </div>
+            <Image className='object-contain' src='https://links.papareact.com/ua6' width={200} height={100} />
         </header>
     );
 };
