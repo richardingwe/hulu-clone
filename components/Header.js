abee09963edc55901c488dcc496a27cc5c1c7eb0
richardingwe <richardingwe5@gmail.com>
2021-05-06 23:44:30 +0100

    added icons into header

--- components/Header.js
+++ components/Header.js
@@ -6,6 +6,11 @@ const Header = () => {
     return (
         <header className=''>
             <HeaderItem title='HOME' Icon={HomeIcon} />
+            <HeaderItem title='TRENDING' Icon={LightningBoltIcon} />
+            <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} />
+            <HeaderItem title='COLLECTIONS' Icon={CollectionIcon} />
+            <HeaderItem title='SEARCH' Icon={SearchIcon} />
+            <HeaderItem title='ACCOUNT' Icon={UserIcon} />
             <Image className='object-contain' src='https://links.papareact.com/ua6' width={200} height={200} />
         </header>
     );
