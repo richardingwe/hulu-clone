a42ac6e346d6335e682b79e1a65e8cd7dbcb4218
richardingwe <richardingwe5@gmail.com>
2021-05-08 13:54:02 +0100

    .

--- components/Results.js
+++ components/Results.js
@@ -1,9 +1,13 @@
-const Results = () => {
+const Results = ({ results }) => {
     return (
         <div>
-
+            {results.map(result => (
+                <Thumbnail key={result.id} result={result} />
+            ))}
         </div>
     );
 };
 
 export default Results;
+
+
--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,32 @@ import Image from 'next/image';
 import Header from '../components/Header';
 import Nav from '../components/Nav';
 import Results from '../components/Results';
+import requests from '../utils/request';
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
         <title>Hulu 3.0</title>
       </Head>
 
-      {/* header */}
       <Header />
-
-      {/* nav */}
       <Nav />
-
-      {/* results */}
-      <Resultsts />
+      <Results results={results} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await
+    fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`)
+      .then(res => res.json());
+
+  return {
+    props: {
+      results: request.results
+    }
+  };
 }
